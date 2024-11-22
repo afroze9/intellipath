fd5f7ed50c7fe19ed761f2178bc3c2d6537431e9
afroze9 <afrozeamjad@gmail.com>
2024-11-22 15:37:01 +0500

    Added qdrant db

--- src/Aspire/IntelliPath.AppHost/Program.cs
+++ src/Aspire/IntelliPath.AppHost/Program.cs
@@ -1,5 +1,7 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+var qdrantApiKey = builder.AddParameter("qdrantApiKey", secret: true);
+
 IResourceBuilder<ProjectResource> elsaServer = builder
     .AddProject<Projects.IntelliPath_Server>("elsa-server");
 
@@ -7,5 +9,7 @@ builder.AddProject<Projects.IntelliPath_Studio>("elsa-studio")
     .WithExternalHttpEndpoints()
     .WithReference(elsaServer)
     .WaitFor(elsaServer);
+
+var qdrant = builder.AddQdrant("memory-db", qdrantApiKey);
 
 builder.Build().Run();
