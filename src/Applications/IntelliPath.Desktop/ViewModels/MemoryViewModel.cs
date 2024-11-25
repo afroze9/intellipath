2362f05ff0fe1307470e0ef53bc21d3dadfde6b1
afroze9 <afrozeamjad@gmail.com>
2024-11-26 00:57:19 +0500

    Memory Page Updated

---
+++ src/Applications/IntelliPath.Desktop/ViewModels/MemoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntelliPath.Desktop.ViewModels;
+
+public class MemoryViewModel
+{
+    [Required]
+    public string? Description { get; set; }
+
+    [Required]
+    public IEnumerable<TagViewModel> Tags { get; set; } = [];
+}
+
+public class TagViewModel
+{
+    [Required] public string Name { get; set; } = string.Empty;
+}
