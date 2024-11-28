ceea14c7c5f6efd1ef6a9ada41d29c6d3a897959
afroze9 <afrozeamjad@gmail.com>
2024-11-29 00:20:55 +0500

    Chat fixed

--- src/Applications/IntelliPath.Desktop/State/AppState.cs
+++ src/Applications/IntelliPath.Desktop/State/AppState.cs
@@ -94,6 +94,16 @@ public class AppState
     }
     
     public event Action? OnChange;
+
+    public void DeleteConversation(string conversationId)
+    {
+        if (_conversations.Any(x => x.Id.ToUpperInvariant() == conversationId.ToUpperInvariant()))
+        {
+            var conversation = _conversations.First(x => x.Id.ToUpperInvariant() == conversationId.ToUpperInvariant());
+            _conversations.Remove(conversation);
+            OnChange?.Invoke();
+        }
+    }
 }
 
 public class UserState
